5089e5c216972f93542bb7b6eb55339434f12b1a
Neal Taylor, Jr <neal.taylorjr@gmail.com>
2016-08-11 14:29:43 -0500

    successful export of app controller.

---
+++ client/controllers/app.js
@@ -0,0 +1,27 @@
+"use strict";
+var App = angular.module('App', []);
+App.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
+        var refreshBoard = function () {
+            $http.get('/events').success(function (response) {
+                $scope.events = response;
+            });
+        };
+        refreshBoard();
+        $scope.moreDetails = function () {
+            $http.get('/events/:id').success(function (response) {
+                $scope.event.detail = response;
+            });
+        };
+        $scope.addEvent = function () {
+            $http.post('/events', $scope.events).success(function (response) {
+                refreshBoard();
+            });
+        };
+    }]);
+var AppCtrl = (function () {
+    function AppCtrl() {
+    }
+    return AppCtrl;
+}());
+exports.AppCtrl = AppCtrl;
+//# sourceMappingURL=app.js.map
