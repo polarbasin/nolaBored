285aaeee84ea3760cc701c88ab52070777b4253d
Neal Taylor, Jr <neal.taylorjr@gmail.com>
2016-08-12 16:30:25 -0500

    add CSS and loading gif

--- client/app.component.ts
+++ client/app.component.ts
@@ -2,48 +2,39 @@ import { Component } from '@angular/core';
 import { AppCtrl } from './controllers/app'
 @Component({
   selector: 'app',
-  template: `<div class="col-md-12" id="header"><h1>BORED</h1></div>
+  template: 
+  `<div class="col-md-12" id="header"><h1>BORED</h1></div>
   <div class="container" ng-controller="AppCtrl">
     <div class="events">
-      <div class="row" ng-repeat="event in events">
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
+      <div class="row">
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
       </div>
-      <div class="row" ng-repeat="event in events">
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
+      <div class="row">
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
       </div>
-      <div class="row" ng-repeat="event in events">
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
+      <div class="row">
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
       </div>
-      <div class="row" ng-repeat="event in events">
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
+      <div class="row">
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
       </div>
-      <div class="row" ng-repeat="event in events">
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
-        <div class="col-md-2">Event</div>
+      <div class="row">
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
       </div>
     </div>
   </div>`
