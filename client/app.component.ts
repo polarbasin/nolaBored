be4251bcaf8f7da1697d64d2c25adb7660d70018
Neal Taylor, Jr <neal.taylorjr@gmail.com>
2016-08-12 19:14:10 -0500

    add css and prep for db.

--- client/app.component.ts
+++ client/app.component.ts
@@ -3,46 +3,16 @@ import { AppCtrl } from './controllers/app'
 @Component({
   selector: 'app',
   template: 
-  `<div class="col-md-12" id="header"><h1>BORED</h1></div>
+  `<div class="col-md-12" id="header"><h1>BORED<span class="question">?</span></h1></div>
   <div class="container" ng-controller="AppCtrl">
     <div class="events">
-      <div class="row">
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-      </div>
-      <div class="row">
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-      </div>
-      <div class="row">
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-      </div>
-      <div class="row">
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-      </div>
-      <div class="row">
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
-        <div class="col-md-3" id="event"><img src="http://fillmurray.com/200/300"></div>
+      <div class="row" ngrepeat="event in events">
+        <div class="col-md-3" ng-mouseover="moreDetails()">{{event}}</div>
+        <div class="col-md-3" ng-mouseover="moreDetails()">{{event}}</div>
+        <div class="col-md-3" ng-mouseover="moreDetails()">{{event}}</div>
+        <div class="col-md-3" ng-mouseover="moreDetails()">{{event}}</div>
       </div>
     </div>
   </div>`
 })
 export class AppComponent { }
-        // <div class="col-md-2" ng-mouseover="moreDetails()">{{event}}</div>
-        // <div class="col-md-2" ng-mouseover="moreDetails()">{{event}}</div>
-        // <div class="col-md-2" ng-mouseover="moreDetails()">{{event}}</div>
-        // <div class="col-md-2" ng-mouseover="moreDetails()">{{event}}</div>
-        // <div class="col-md-2" ng-mouseover="moreDetails()">{{event}}</div>
-        // <div class="col-md-2" ng-mouseover="moreDetails()">{{event}}</div>
