1380a6690c342031aaa3f9e09e6a7279a1119bff
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-20 13:03:10 -0500

    (style) fix syntax

--- server/controllers/eventcontroller.js
+++ server/controllers/eventcontroller.js
@@ -5,20 +5,16 @@ const getImgUrl = str => {
   return results ? results[2] : '../styles/placeholder.png';
 };
 
-// 'https://placehold.it/225x175'
-
-const saveEvent = (createdEvent) => {
+const saveEvent = createdEvent => {
   var event = new Event({
     title: createdEvent.title,
     link: createdEvent.link,
     description: createdEvent.description,
     imgUrl: getImgUrl(createdEvent.description),
   });
-  event.save(function(err, result){
-    //res.json(result)
+  event.save(err => {
     if (err) return console.error('err');
   });
 };
 
 module.exports = saveEvent;
-
