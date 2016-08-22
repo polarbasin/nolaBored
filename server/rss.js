fa5cfb96d489fe3ab5170a41beabff09ebb78a23
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-21 23:11:29 -0500

    (fix) promisify parseXmlString for readability; save events to DB

--- server/rss.js
+++ server/rss.js
@@ -1,33 +1,20 @@
-//makes request and parses data
+// makes request and parses data
 var rp = require('request-promise');
-let parseXmlStr = require('xml2js').parseString;
+let Promise = require('bluebird');
+let parseXmlStr = Promise.promisify(require('xml2js').parseString);
 
-var saveEvent = Promiserequire('./controllers/eventcontroller');
+var saveEvent = require('./controllers/eventcontroller');
 
-// for rss Feeds
-module.exports.requestRSS = function(feed){
-  rp(feed)
-  .then(xml => {
-    return new Promise((resolve, reject) => {
-      parseXmlStr(xml, (err, events) => {
-        if (err) {
-          reject(err);
-        } else {
-          console.log('events', events);
-          resolve(events);
-        }
-      });
+module.exports.requestRSS = function(feedUrl){
+  rp(feedUrl)
+  .then(parseXmlStr)
+  .then(results => {
+    let feed = results.rss.channel[0];
+    return feed.item.forEach(item => {
+      saveEvent(item);
     });
-  }).catch(err => {
+  })
+  .catch(err => {
     console.error('RSS request error:', err);
   });
-
-  // parser.on('title', function(title) {
-  //   console.log('title of feed is', title);
-  // });
-  // parser.on('item', function(item) {
-  //   // save parsedXML to DB
-  //   saveEvent(item);
-  // });
-
 };
