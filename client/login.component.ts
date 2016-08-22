874072beac1b6950c75dc6b75ce16d563de2b480
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-22 16:53:52 -0500

    (feat) add login-form and login components

---
+++ client/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  template: `
+    <div>
+      <login-form></login-form>
+    </div>
+  `
+})
+
+export class LoginComponent { }
