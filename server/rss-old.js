2a0ad3ad3047cb1f388f25e15f05298c07566528
Harvey Sanders <sanders.harvey@gmail.com>
2016-08-21 22:09:34 -0500

    (fix) stop saving rss xml to disk before parsing

---
+++ server/rss-old.js
@@ -0,0 +1,42 @@
+//makes request and parses data
+var saveEvent = require('./controllers/eventcontroller');
+var rp = require('request-promise');
+var FeedMe = require('feedme')
+  , parser = new FeedMe()
+  , fs = require('fs');
+
+// for rss Feeds
+module.exports.requestRSS = function(feed){
+  rp(feed)
+  .then(htmlString => {
+    console.log('request worked');
+    // writing results to disk?
+    fs.writeFile('read.txt', htmlString, 'utf8', ()=>{});
+  }).then(()=>{
+    // reading file to pipe to parser
+    // probably could just pipe xmlString to parser directly
+    fs.createReadStream('read.txt').pipe(parser);
+  }).catch(err => {
+    console.log('error:', err);
+  });
+
+  parser.on('title', function(title) {
+    console.log('title of feed is', title);
+  });
+  parser.on('item', function(item) {
+    // save parsedXML to DB
+    saveEvent(item);
+  });
+
+};
+
+// for api request 
+module.exports.request = function(){
+
+  rp('http://www.houseofblues.com/neworleans/api/EventCalendar/GetEvents?startDate=8/15/2016&endDate=9/1/2016&venueIds=221834&limit=30&offset=1&genre=&artist=&offerType=')
+    .then(results => {
+      console.log('request 2 worked');
+    }).catch(err => {
+      console.log('error:', err);
+    });
+};
--- server/rss.js
+++ server/rss.js
@@ -1,42 +1,33 @@
 //makes request and parses data
-var saveEvent = require('./controllers/eventcontroller');
 var rp = require('request-promise');
-var FeedMe = require('feedme')
-  , parser = new FeedMe()
-  , fs = require('fs');
+let parseXmlStr = require('xml2js').parseString;
+
+var saveEvent = Promiserequire('./controllers/eventcontroller');
 
 // for rss Feeds
 module.exports.requestRSS = function(feed){
   rp(feed)
-  .then(htmlString => {
-    console.log('request worked');
-    // writing results to disk?
-    fs.writeFile('read.txt', htmlString, 'utf8', ()=>{});
-  }).then(()=>{
-    // reading file to pipe to parser
-    // probably could just pipe xmlString to parser directly
-    fs.createReadStream('read.txt').pipe(parser);
-  }).catch(err => {
-    console.log('error:', err);
-  });
-
-  parser.on('title', function(title) {
-    console.log('title of feed is', title);
-  });
-  parser.on('item', function(item) {
-    // save parsedXML to DB
-    saveEvent(item);
-  });
-
-};
-
-// for api request 
-module.exports.request = function(){
-
-  rp('http://www.houseofblues.com/neworleans/api/EventCalendar/GetEvents?startDate=8/15/2016&endDate=9/1/2016&venueIds=221834&limit=30&offset=1&genre=&artist=&offerType=')
-    .then(results => {
-      console.log('request 2 worked');
-    }).catch(err => {
-      console.log('error:', err);
+  .then(xml => {
+    return new Promise((resolve, reject) => {
+      parseXmlStr(xml, (err, events) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log('events', events);
+          resolve(events);
+        }
+      });
     });
+  }).catch(err => {
+    console.error('RSS request error:', err);
+  });
+
+  // parser.on('title', function(title) {
+  //   console.log('title of feed is', title);
+  // });
+  // parser.on('item', function(item) {
+  //   // save parsedXML to DB
+  //   saveEvent(item);
+  // });
+
 };
